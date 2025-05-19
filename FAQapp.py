7e02de8383e6c2413be7202e7870576327fca226
madhumanti-banerjee <madhumanti.banerjee@sky.uk>
2025-05-19 14:35:14 +0530

    Update FAQapp.py

--- FAQapp.py
+++ FAQapp.py
@@ -6,7 +6,6 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
-vectorstore = Chroma.from_documents(texts, embedding=embeddings)
 import time
 
 # Streamlit config
